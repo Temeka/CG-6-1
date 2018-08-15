ac0b0372438bea04fb85f94c6ef0e350eef35017
Adriana Disano <adrianadisano@gmail.com>
2018-08-15 14:17:13 -0500

    -Created.

--- FullName_2/Program.cs
+++ FullName_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace FullName_2
 {
@@ -6,7 +7,33 @@ namespace FullName_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //acquire the first name from the user
+            Console.WriteLine("Please enter your first name:");
+            string firstName = Console.ReadLine();
+
+            //acquire the last name from the user
+            Console.WriteLine("Please enter your last name:");
+            string lastName = Console.ReadLine();
+
+            //write the first and last name
+            Console.WriteLine(GetFirstName(firstName, lastName));
+
+            //keep the program open
+            Console.ReadLine();
+
+        }
+        private static string GetFirstName(string firstName, string lastName)
+        {
+            //initialize a StringBuilder
+            StringBuilder fullName = new StringBuilder();
+            //add the first and last name to the StringBuilder
+            fullName.Append(firstName);
+            fullName.Append(" ");
+            fullName.Append(lastName);
+            //convert the first and last name from StringBuilder to string
+            string fullNameStr = fullName.ToString();
+            //pass the string back to Main
+            return fullNameStr;
         }
     }
 }
