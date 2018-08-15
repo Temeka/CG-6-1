9b79e7e8405b16aba909fcb6b2ed414e2f5762f6
Adriana Disano <adrianadisano@gmail.com>
2018-08-15 14:02:52 -0500

    Add project files.

---
+++ FullName_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FullName_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
